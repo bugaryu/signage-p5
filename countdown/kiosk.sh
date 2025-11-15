dcd31b90b8803a3a06fe5e7aa5ef99e4735bd841
bugaryu <ryu.ishzk@gmail.com>
2025-11-15 21:25:52 +0900

    kioskモードの設定変更

--- countdown/kiosk.sh
+++ countdown/kiosk.sh
@@ -1,8 +1,6 @@
-# chromium --app="file:///home/montech/workspace/signage/countdown/index.html"
-
 chromium \
   --kiosk \
   --noerrdialogs \
   --disable-infobars \
   --incognito \
-  "file:///home/montech/workspace/signage/countdown/index.html"
+  "file:///home/montech/workspace/signage/countdown/time.html"
